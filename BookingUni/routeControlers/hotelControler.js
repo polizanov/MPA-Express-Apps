ae9fb37ae5656d1370796c30860a216816ac45e2
Darin Polizanov <72756517+polizanov@users.noreply.github.com>
2021-06-09 17:49:07 +0300

    created book functionality

--- BookingUni/routeControlers/hotelControler.js
+++ BookingUni/routeControlers/hotelControler.js
@@ -21,9 +21,30 @@ router.post("/create", async (req, res) => {
 router.get("/details/:hotelId", async (req, res, next) => {
     try {
         let data = await hotelService.getHotelById(req.params.hotelId, res.locals.user._id);
+        console.log(data);
+        console.log(res.locals.user._id);
         res.render("crud/details", { title: "Details", data })
     } catch {
         next();
+    }
+})
+
+router.get("/edit/:hotelId", async (req, res, next) => {
+    try {
+        let data = await hotelService.getHotelById(req.params.hotelId);
+        res.render("crud/edit", { title: "Edit", data });
+    } catch {
+        next()
+    }
+})
+
+router.post("/edit/:hotelId", async (req, res) => {
+    try {
+        await hotelService.editHotel(req.body, req.params.hotelId)
+        res.redirect(`/hotel/details/${req.params.hotelId}`);
+    } catch (err) {
+        console.log(err)
+        res.render("crud/edit", { title: "Edit", err })
     }
 })
 
@@ -32,6 +53,16 @@ router.get("/delete/:hotelId", async (req, res, next) => {
         await hotelService.deleteHotel(req.params.hotelId);
     } catch {
         next()
+    }
+})
+
+router.get("/book/:hotelId", async (req, res, next) => {
+    try {
+        await hotelService.bookHotel(res.locals.user._id, req.params.hotelId);
+        res.redirect(`/hotel/details/${req.params.hotelId}`);
+    } catch (err){
+        console.log(err);
+        next();
     }
 })
 
--- BookingUni/services/hotelServices.js
+++ BookingUni/services/hotelServices.js
@@ -1,4 +1,5 @@
 const Hotel = require("../schemes/Hotel");
+const User = require("../schemes/User");
 
 async function create(data, userId) {
     if (data.name == "" || data.city == "" || data.freeRooms == "" || data.imageUrl == "") {
@@ -38,19 +39,82 @@ function getAll() {
     return Hotel.find({}).lean();
 }
 
-function getHotelById(id, userId) {
-    return Hotel.findOne({ _id: id })
+async function getHotelById(id, userId) {
+    let data = await Hotel.findOne({ _id: id })
         .lean()
         .map(x => Object.assign(x, { isAuthor: x.ownerId == userId }))
+
+    data.usersBooked.forEach(curUser => {
+        if (curUser == userId) {
+            data = Object.assign(data, { isBooked: true })
+        }
+    })
+
+    return data;
 }
 
 function deleteHotel(id) {
     return Hotel.deleteOne({ _id: id });
 }
 
+function editHotel(data, id) {
+    if (data.name == "" || data.city == "" || data.freeRooms == "" || data.imageUrl == "") {
+        throw { message: "All fields are requred" }
+    }
+
+    if (data.title < 4) {
+        throw { message: "Title should be at least 4 characters" }
+    }
+
+    if (data.name < 4) {
+        throw { message: "Description should be at least 20 characters" }
+    }
+
+    if (!data.imageUrl.startsWith("http") || !data.imageUrl.startsWith("https")) {
+        throw { message: "ImageUrl should be starts with http or https" }
+    }
+
+    if (Number(data.freeRooms) < 1 || Number(data.freeRooms) > 100) {
+        throw { message: "Free rooms should be between 1 and 100" };
+    }
+
+    let dataObj = {
+        name: data.name,
+        city: data.city,
+        imageUrl: data.imageUrl,
+        freeRooms: data.freeRooms,
+    }
+
+
+    return Hotel.updateOne({ _id: id }, dataObj)
+}
+
+async function bookHotel(userId, hotelId) {
+    [hotelData, userData] = await Promise.all([
+        Hotel.findOne({ _id: hotelId }),
+        User.findOne({ _id: userId })
+    ])
+
+    hotelData.usersBooked.push(userId);
+    userData.bookedHotels.push(hotelId);
+
+    if (hotelData.freeRooms <= 0) {
+        throw { message: 'Sorry, all rooms are occupied' };
+    }
+
+    hotelData.freeRooms--;
+
+    return Promise.all([
+        Hotel.updateOne({ _id: hotelId }, hotelData),
+        User.updateOne({ _id: userId }, userData)
+    ])
+}
+
 module.exports = {
     create,
     getAll,
     getHotelById,
-    deleteHotel
+    deleteHotel,
+    editHotel,
+    bookHotel
 }
